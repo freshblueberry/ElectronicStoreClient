44abe605b4c432b442fc9189b9964cba8c6ca86a
Clifford <choiclifford@gmail.com>
2021-05-12 23:38:03 -0700

    minor addition to cart and Layout

--- ElectronicStoreClient/Cart.aspx.vb
+++ ElectronicStoreClient/Cart.aspx.vb
@@ -1,5 +1,11 @@
-﻿Public Class Cart
+﻿Imports System.Data
+
+
+Public Class Cart
     Inherits System.Web.UI.Page
+    Public strCartNo As String = " "
+    Dim CookieBack As HttpCookie = HttpContext.Current.Request.Cookies("CartNo")
+
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
--- ElectronicStoreClient/Layout.Master.vb
+++ ElectronicStoreClient/Layout.Master.vb
@@ -2,7 +2,13 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If Session("Customer") <> "" Then
+            'Login button is visable. this might be required later'
+            hlLogin.Visible = False
+            hlLogout.Visible = True
+            lblCustomer.Text = Session("Customer")
+            lblCustomer.Visible = True
+        End If
     End Sub
 
 End Class
